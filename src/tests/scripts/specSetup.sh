439f1b2d18feebff146d5720ab1cfaed10eedaf4
C.A.B. <git@helpchoice.com>
2025-12-17 20:59:05 -0500

    Make test work the first time without pre-install archive

--- src/tests/scripts/specSetup.sh
+++ src/tests/scripts/specSetup.sh
@@ -20,11 +20,13 @@ setup() {
   [ -e "test/$subf" ] || mkdir -p test/$subf
   [ -e "distributions" ] || { echo "There is no distributions folder"; exit 249; }
   [ $(ls -1 distributions/HALDiSh-*.sh 2>/dev/null | wc -l) -eq 1 ] || { echo "There is not exactly 1 distribution script"; exit 250; }
-
   if [ $(ls -1 deployed/*.sh 2>/dev/null | wc -l) -le 1 ]
   then
     cp distributions/HALDiSh-*.sh deployed
-    $(cd deployed; ./HALDiSh-*.sh)
+
+    cd "deployed"
+    ./HALDiSh-*.sh
+    cd -
   fi
 
   . deployed/validator.sh >/dev/null
